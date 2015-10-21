8fdc9d2e0fd9092ba08b41c72f06e4de89f67f4a
carolynworldwide <carolynworldwide@gmail.com>
2015-10-21 11:36:43 -0700

    saved file on refreshing local storage

--- js/communications.js
+++ js/communications.js
@@ -76,22 +76,15 @@ var Sab = new CList('SABRINA', 24, "FEMALE", "511 Boren Ave, Seattle, WA", "0123
 var submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click', newUserSubmit);
 
-// function populateStorage() {
-//   for (var i = 0; i<localStorage.length; i+=1){
-//     localStorage.getItem(localStorage.key(i));
-//     console.log(localStorage.getItem(localStorage.key(i)));
-//     var infoRow = document.createElement('tr');
-//     var contentInfoContact = document.createElement('td');
-//     contentInfoContact.appendChild(document.createTextNode(JSON.parse(localStorage.getItem(localStorage.key(i)))));
-//     infoRow.appendChild(contentInfoContact);
-//     contactColumn.appendChild(infoRow);
-//   }
-// }
-
-
 function populateStorage() {
-  for (var i = 0; i < localStorage.length; i += 1) {
-    console.log(JSON.parse(localStorage[i]))
+  for (var i = 0; i<localStorage.length; i+=1){
+    localStorage.getItem(localStorage.key(i));
+    console.log(localStorage.getItem(localStorage.key(i)));
+    var infoRow = document.createElement('tr');
+    var contentInfoContact = document.createElement('td');
+    contentInfoContact.appendChild(document.createTextNode(JSON.parse(localStorage.getItem(localStorage.key(i)))));
+    infoRow.appendChild(contentInfoContact);
+    contactColumn.appendChild(infoRow);
   }
 }
 populateStorage();
