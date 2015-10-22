24f0bd1c18c8c71413ee171a51c940b0335ef851
Aaron Filson <aaronfilson@gmail.com>
2015-10-22 10:29:08 -0700

    Page 2 now has info reloaded from local storage.

--- js/form.js
+++ js/form.js
@@ -1,4 +1,3 @@
-var submitButtonOnPageTwo = document.getElementById('oneButton');
 
 var cList = {
   allMyTables: []
@@ -8,9 +7,7 @@ var pList = {
 	houseList: [false, "Placeholder for the list of things to check for in preparation for an earthquake"],
 	aprtList: [false, "Placeholder for apartments"],
 	driveList: [false, "Placeholder for a driving checklist"],
-	medList: [false, "Placeholder for a medication checklist"],
-
-	//listOfChecks: [this.houseList, this.aprtList, this.driveList, this.medList],	
+	medList: [false, "Placeholder for a medication checklist"]
 };
 
 var UserInfo = function () {
@@ -19,26 +16,33 @@ var UserInfo = function () {
 	this.username = '';
 };
 
-var localObj = new UserInfo();
-localObj.username = 'Sabrina';
-
 UserInfo.prototype.getFromStore = function(username) {
 	// key is username
+	console.log('in getFromStore function');
 	var temp = localStorage.getItem(username);
 	var unstringedTemp;
-	if(temp) {
+	if(temp != null) {
+		console.log('temp var is not null, and evaluates to: ');
+		console.dir(temp);
     	unstringedTemp = JSON.parse(temp);
 	  	this.personalizedList = unstringedTemp.personalizedList;
+	  	pList = unstringedTemp.personalizedList;
 	  	this.commList = unstringedTemp.commList;
+	  	cList = unstringedTemp.commList;
   		displayTheLists();
   	}
+  	console.log('at the end of the getFromStore function, and the value of this is: ');
+  	console.dir(this);
 };
 
 UserInfo.prototype.setToStore = function() {
+	console.log('in setToStore function');
 	localStorage.setItem(this.username, JSON.stringify(this));
 };
 
 UserInfo.prototype.updateForm = function() {
+	console.log('in the updateForm function, pList is : ');
+	console.dir(pList);
 	var htmlInfo = document.createElement("ul");
 	var checkBoxOnPage = [];
 	var temp;
@@ -73,11 +77,17 @@ UserInfo.prototype.updateForm = function() {
 		temp.textContent = "A start to preparing your required medication.";
 		htmlInfo.appendChild(temp);
 	}
-	
-	console.log(event); 
+	console.log('at the end of updateForm function, the pList is: ')
+	console.dir(pList); 
 	document.getElementById("customPrepList").appendChild(htmlInfo);
 	loopback();//make the local store happen
 };
+
+var submitButtonOnPageTwo = document.getElementById('oneButton');
+
+var localObj = new UserInfo();
+localObj.username = 'Sabrina';
+
 
 function displayTheLists () {
 	console.log('in the displayTheLists function');
@@ -85,9 +95,12 @@ function displayTheLists () {
 }
 
 function loopback () {
+	console.log('in loopback function')
 	localObj.personalizedList = pList;
 	localObj.setToStore();
 }
 
 localObj.getFromStore(localObj.username); // Every time the page loads, call up the last saved info
+console.log('Reaching the end of the js file, and the current value of the UserInfo instance localObj is: ');
+console.dir(localObj); //show us what the loaded object is
 submitButtonOnPageTwo.addEventListener('click', localObj.updateForm);
