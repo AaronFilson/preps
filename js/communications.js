629dd61e3a18f2429deeeaaaab0f91b7cae770d6
Aaron Filson <aaronfilson@gmail.com>
2015-10-23 10:17:49 -0700

    Code cleanup and removal of console stuff2.

--- js/communications.js
+++ js/communications.js
@@ -1,11 +1,10 @@
-// HEADER OF CONTACT INFO TABLE START
+// HEADER OF CONTACT INFO TABLE
 var contactColumn = document.getElementById("contactInfo");
 var headerRow = document.createElement('tr');
 var newInfoColumn = document.createElement('th');
 newInfoColumn.appendChild(document.createTextNode("Name"));
 headerRow.appendChild(newInfoColumn);
 contactColumn.appendChild(headerRow);
-console.log("Contact Table Header running");
 
 var categories = ['Age', 'Gender', 'Address', 'Contact Number', 'Meet Up Location', 'Additional Info'];
 
@@ -13,9 +12,8 @@ for (var i=0; i<categories.length; i++) {
   var categoriesHeader = document.createElement('th');
   categoriesHeader.appendChild(document.createTextNode(categories[i]));
   headerRow.appendChild(categoriesHeader);
-  console.log("Categories header working");
 }
-// OBJECT CONSTRUCTOR FOR EACH START
+// OBJECT CONSTRUCTOR FOR EACH USER
 var TableMaker = function(username, age, gender, address, contactNo, meetUpLoc, extraInfo) {
   this.username = username;
   this.age = age;
@@ -25,20 +23,18 @@ var TableMaker = function(username, age, gender, address, contactNo, meetUpLoc, 
   this.meetUpLoc = meetUpLoc;
   this.extraInfo = extraInfo;
   this.infoArray = [age, gender, address, contactNo, meetUpLoc, extraInfo];
-  //CREATING TABLE ROW FOR NEW 
+
   var infoRow = document.createElement('tr');
   var userNameRow = document.createElement('td');
   userNameRow.appendChild(document.createTextNode(this.username));
   infoRow.appendChild(userNameRow);
   contactColumn.appendChild(infoRow);
-  console.log("Place row working");
-  //PUSHING OBJECT TO TABLE
+
   for (var i=0; i<categories.length; i++) {
     var contentInfoContact = document.createElement('td');
     contentInfoContact.appendChild(document.createTextNode(this.infoArray[i]));
     infoRow.appendChild(contentInfoContact);
-    console.log("Pushing data of object properly");
-    }
+  }
 };
 //NEW USER SUBMIT FUNCTION
 var newUserSubmit = function(e) {
@@ -50,19 +46,12 @@ var newUserSubmit = function(e) {
   var newContactNo = document.getElementById('contactNo');
   var newMeetUpLoc = document.getElementById('meetUpLoc');
   var newExtraInfo = document.getElementById('extraInfo');
-  var idOfTheHtmlTable = document.getElementById('contactInfo');
-//CREATING NEW OBJECT
-  console.log("allMyTables before push: ");
-  console.dir(cList.allMyTables);
-  cList.allMyTables.push(new TableMaker((newUser.value.toUpperCase()), newAge.value, (newGender.value.toUpperCase()), newAddress.value, newContactNo.value, newMeetUpLoc.value, newExtraInfo.value, idOfTheHtmlTable));
-  console.log("allMyTables after push: ");
-  console.dir(cList.allMyTables);
+
+  cList.allMyTables.push(new TableMaker((newUser.value.toUpperCase()), newAge.value, (newGender.value.toUpperCase()), newAddress.value, newContactNo.value, newMeetUpLoc.value, newExtraInfo.value));
   profile = newUser.value.toUpperCase();
-  console.log("New profile name: "+profile);
-  console.log("Submit form working");
-//CALLING THE SAVE TO LOCAL STORAGE
+
   setToStore();
-//RESETTING FORM
+
   newUser.value = null;
   newAge.value = null;
   newGender.value = null;
@@ -71,43 +60,44 @@ var newUserSubmit = function(e) {
   newMeetUpLoc.value = "Primary: Secondary:";
   newExtraInfo.value = null;
 };
-//EVENT HANDLER TO SUBMIT BUTTON
+// var Sab = new TableMaker('SABRINA', 24, "FEMALE", "511 Boren Ave, Seattle, WA", "0123456789", "Primary: UW Secondary: SEATAC", "");
 var submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click', newUserSubmit);
+//LOCAL STORAGE
+var pList = {
+};
 
-//CREATING PAGE4 LOCAL STORE UNDER USERINFO
 var cList = {
   allMyTables: []
 };
-console.log("allMyTables after create "+cList.allMyTables);
-//PROFILE CONSTRUCTOR
-var UserInfo = function () {
-    this.personalizedList = pList;
-    this.commList = cList;
-    this.profileName = '';
-    console.log("UserInfo is running");
-};
-//VARIABLE THAT CREATES A NEW PROFILE CONSTRUCTOR
-var localObj = new UserInfo();
-localObj.profileName = "SABRINA";
 
-//FUNCTION TO GET FROM LOCAL STORAGE
-UserInfo.prototype.getFromStore = function (username) {
-    var temp = localStorage.getItem(username);
-    var unstringedTemp;
-    if(temp != null) {
-      unstringedTemp = JSON.parse(temp);
-      this.commList = unstringedTemp.commList;
-      cList = unstringedTemp.commList;
-      reMakeTable(cList.allMyTables);
-    }
-  console.log("allMyTables in getFromStore: ");
-  // console.dir(tableStick.commList.allMyTables);
-  console.log("getFromStore working");
+var UserInfo = function () {
+  this.personalizedList = pList;
+  this.commList = cList;
+  this.profile = 'SABRINA';
 };
-localObj.getFromStore(this.profileName);
-function reMakeTable(littleArray) {
-    for (var i=0; i<littleArray.length; i+=1) {
-      var superTemp = TableMaker(littleArray[i].username, littleArray[i].age, littleArray[i].gender, littleArray[i].address, littleArray[i].contactNo, littleArray[i].meetUpLoc, littleArray[i].extraInfo)
-    }
+
+var localObj = new UserInfo();
+
+function setToStore() {
+  var temp = JSON.stringify(localObj);
+  localStorage.setItem(localObj.profile, temp);
+};
+
+UserInfo.prototype.getFromStore = function (username) {
+  var temp = localStorage.getItem(username);
+  var unstringedTemp;
+  if(temp != null) {
+    unstringedTemp = JSON.parse(temp);
+    this.commList = unstringedTemp.commList;
+    cList = unstringedTemp.commList;
+    reMakeTable(cList.allMyTables);
+  }
+};
+localObj.getFromStore("SABRINA");
+
+function reMakeTable(temp) {
+  for (var i=0; i<temp.length; i+=1) {
+    var superTemp = TableMaker(temp[i].username, temp[i].age, temp[i].gender, temp[i].address, temp[i].contactNo, temp[i].meetUpLoc, temp[i].extraInfo)
+  }
 };
